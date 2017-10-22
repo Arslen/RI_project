fadf70d256fb7002984a6e84cdadd94d3ce00390
Arslen <arslen.r@gmail.com>
2017-10-22 03:04:12 +0200

    First commit

---
+++ functions.py
@@ -0,0 +1,64 @@
+import re
+import math
+from collections import defaultdict
+
+########## Miscellaneous functions ##########
+def findWholeWord(w):
+    return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
+
+
+
+
+#############################################
+
+
+
+##################   LTN   ##################
+
+def TFListLTN(l, list_doc):
+    lis = defaultdict(list)
+    #final_ls = [0] * len(l)
+    #c = 0
+    for doc in list_doc:
+        for i in l:
+            count = sum(1 for _ in re.finditer(r'\b%s\b' % re.escape(i), doc.contents[1]))
+            if count != 0:
+                lis[l.index(i)].append(1 + math.log(count))
+            else:
+                lis[l.index(i)].append(1)
+    return lis
+    # for x in ls:
+    #     if x != 0:
+    #         final_ls[c] = 1 + math.log(ls[c])
+    #     else:
+    #         final_ls[c] = 0
+
+def IDFListLTN(l, number_documents, list_doc):
+    ls = [0] * len(l)
+    final_ls = [0] * len(l)
+    c = 0
+    for doc in list_doc:
+        for i in l:
+            if findWholeWord(i)(doc.contents[1]):
+                ls[l.index(i)] += 1
+    for x in ls:
+        if x != 0:
+            final_ls[c] = 1 + math.log(number_documents/x)
+            c += 1
+        else:
+            final_ls[c] = 1 + math.log(number_documents)
+            c += 1
+    return final_ls
+
+
+
+#############################################
+
+
+
+##################   LTC   ##################
+
+
+
+
+#############################################
---
+++ main.py
@@ -0,0 +1,53 @@
+from bs4 import BeautifulSoup
+import re
+import math
+from functions import *
+
+
+list0 = ["olive","oil","health","benefit"]
+list1 = ["notting","hill","film","actors"]
+list2 = ["probabilistic","models","in","information","retrieval"]
+list3 = ["web","link","network","analysis"]
+list4 = ["web","ranking","scoring","algorithm"]
+list5 = ["supervised","machine","learning","algorithm"]
+list6 = ["operating","system","mutual","exclusion"]
+biglist =   ['algorithm','benefit','operating','supervised','film','actors',
+            'learning','analysis','link','models','system','machine',
+            'exclusion','information','retrieval','health','oil','mutual',
+            'network','ranking','hill','probabilistic','olive','notting','in',
+            'web','scoring']
+
+
+list_requests = ["2009011", "2009036", "2009067", "2009073", "2009074", "2009078", "2009085"]
+number_request = 0
+number_result = 1
+#./Text_Only_Ascii_Coll_MWI_NoSem
+
+with open("./Text_Only_Ascii_Coll_MWI_NoSem") as fp:
+    soup = BeautifulSoup(fp, "lxml")
+    number_documents = len(soup.find_all("doc"))
+    # with open("./results", "w") as res:
+    #     res.write(
+    #             list_requests[number_request] + " " +
+    #             "Q0" + " " +
+    #              + " " +
+    #             number_result + " " +
+    #             score + " " +
+    #             "ArslenMarouane" + " " +
+    #             "/article[1]" + "\n"
+    #              )
+
+    for key,val in (TFListLTN(biglist, soup.find_all("doc"))).items():
+        print (key, "=>", val)
+
+    #print (IDFList(list0, number_documents, soup.find_all("doc")))
+
+    # for doc in (soup.find_all("doc")):
+    #     for i in
+    #     if findWholeWord('oil')(doc.contents[1]):
+    #         print ("Zbrah")
+        #count = sum(1 for _ in re.finditer(r'\b%s\b' % re.escape("oil"), doc.contents[1]))
+        #print (count)
+
+        #print(doc.contents[1])
+        #doc.find("oil")
